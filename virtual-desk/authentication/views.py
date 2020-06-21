cacdc96fe8e10bf9f677bb0b4fe24c583e1c575f
Ayush Patel <49319802+ayushpatel14@users.noreply.github.com>
2020-06-21 20:26:59 +0530

    Added login

--- virtual-desk/authentication/views.py
+++ virtual-desk/authentication/views.py
@@ -15,11 +15,27 @@ firebase = pyrebase.initialize_app(config)
 
 authe = firebase.auth()
 database = firebase.database()
+
 def signIn(request):
+
     return render(request, "authentication/signIn.html")
 
 
-def postsign(request):
+def employeepostsign(request):
+    email = request.POST.get('email')
+    passw = request.POST.get('pass')
+    try:
+        user = authe.sign_in_with_email_and_password(email, passw)
+    except:
+        message = "invalid credentials"
+        return render(request, "authentication/signIn.html", {"messg": message})
+    
+    print(user['idToken'])
+    session_id = user['idToken']
+    request.session['uid']=str(session_id)
+    return render(request, "intro/home.html", {"e": email})
+
+def ownerpostsign(request):
     email = request.POST.get('email')
     passw = request.POST.get('pass')
     try:
@@ -38,26 +54,63 @@ def logout(request):
     return render(request, 'authentication/signIn.html')
 
 
-def signUp(request):
-
+def employesignUp(request):
     return render(request, 'authentication/signup.html')
 
-
-def postsignup(request):
+def employeepostsignup(request):
 
     name = request.POST.get('name')
     email = request.POST.get('email')
-    passw = request.POST.get('pass')
+    passw = request.POST.get('password')
+
+    if request.POST.get('re-password') != passw:
+        message = "Password Does not match"
+        return render(request, "authentication/signup.html",{"messg":message})
+
+    companyid = request.POST.get('companyID')
+    companyname = request.POST.get('companyName')
+
+    department = request.POST.get('department')
+    designation = request.POST.get('designation')
 
     try:
         user = authe.create_user_with_email_and_password(email, passw)
         uid = user['localId']
-        data = {"name": name, "status": "1"}
-        database.child("users").child(uid).child("details").set(data)
+        data = {"name": name, "companyid": companyid, "department":department, "designation": designation,"workid":[] }
+        database.child("user").child(uid).set(data)
     except:
         message = "Unable to create account try again"
         return render(request, "authentication/signup.html",{"messg":message})
 
-
     return render(request, "authentication/signIn.html")
 
+def ownerpostsignup(request):
+    from random import randint
+
+    name = request.POST.get('firstName')+' '+request.POST.get('lastName')
+    email = request.POST.get('Email')
+    passw = request.POST.get('Password')
+
+    if request.POST.get('re-password') != passw:
+        message = "Password Does not match"
+        return render(request, "authentication/signup.html",{"messg":message})
+    
+    companyname = request.POST.get('companyName')
+    category = request.POST.get('companyCategory')
+    departments = request.POST.get('companyDepartments')
+
+    if companyname not in database.child('company_data').get().key():
+        companyid = str(companyname)
+    else:
+        companyid = str(companyname)+'_'+str(randint(100,999))
+    
+    try:
+        user = authe.create_user_with_email_and_password(email, passw)
+        uid = user['localId']
+        data = {"name": name, "companyid": companyid, "department":department, "workid":[] }
+        database.child("user").child(uid).set(data)
+    except:
+        message = "Unable to create account try again"
+        return render(request, "authentication/signup.html",{"messg":message})
+
+    return render(request, "authentication/signIn.html")
